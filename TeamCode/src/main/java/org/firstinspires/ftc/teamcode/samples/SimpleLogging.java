1d9f8e1bbfb77623ba82c6d7cd9283bed50251b9
Shaun Voigt <shaunv.1000@gmail.com>
2020-10-02 21:48:30 +0930

    Added a sample demonstrating RobotLog

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/samples/SimpleLogging.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.RobotLog;
+
+@TeleOp(name = "LoggingTest", group = "Samples")
+public class SimpleLogging extends OpMode {
+
+    long loopCount = 0;
+
+    @Override
+    public void init() {
+        RobotLog.logDeviceInfo();
+        RobotLog.dd("TEST", "init");
+    }
+
+    @Override
+    public void loop() {
+        loopCount++;
+
+        if (loopCount % 1000 == 0) {
+            RobotLog.dd("TEST", "Loop count: %d", loopCount);
+        }
+    }
+}

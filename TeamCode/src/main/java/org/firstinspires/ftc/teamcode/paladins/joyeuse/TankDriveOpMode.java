b3aaf9e58afd46dda344fc49643ef75ab85d7c7b
Caleb Rodgers <24555386+calebrodgers@users.noreply.github.com>
2020-11-16 14:12:34 +1030

    Everything works :)
    - SteerDrive works!
    - TankDrive works!
    - Half speed button works!
    - Harvester works!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/paladins/joyeuse/TankDriveOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/paladins/joyeuse/TankDriveOpMode.java
@@ -10,6 +10,7 @@ import org.firstinspires.ftc.teamcode.paladins.joyeuse.JoyeuseShoot;
 public class TankDriveOpMode extends PaladinsOpMode {
     private JoyeuseConfiguration config;
     private JoyeuseDrive drive;
+    private JoyeuseHarvest harvest;
     private JoyeuseShoot shoot;
     private JoyeuseTankDrive tankDrive;
 
@@ -20,7 +21,8 @@ public class TankDriveOpMode extends PaladinsOpMode {
         config = JoyeuseConfiguration.newConfig(hardwareMap, telemetry);
 
         drive = new JoyeuseDrive(this, config.leftMidMotor, config.leftBackMotor, config.rightMidMotor, config.rightBackMotor);
-        shoot = new JoyeuseShoot(this, config.leftShooterMotor, config.rightShooterMotor, config.shooterServo);
+        harvest = new JoyeuseHarvest(this, config.intakeMotor, config.bumpMotor);
+//        shoot = new JoyeuseShoot(this, config.leftShooterMotor, config.rightShooterMotor, config.shooterServo);
         tankDrive = new JoyeuseTankDrive(this, gamepad1, drive);
 
         shootMode = 0;
@@ -30,33 +32,36 @@ public class TankDriveOpMode extends PaladinsOpMode {
     protected void activeLoop() throws InterruptedException {
         tankDrive.update();
 
-        if (gamepad1.left_trigger > 0) {
-            shoot.setServoPower(gamepad1.left_trigger);
-        } else if (gamepad1.right_trigger > 0) {
-            shoot.setServoPower(-gamepad1.right_trigger);
-        } else {
-            shoot.setServoPower(0);
-        }
+        harvest.setIntakePower(gamepad1.right_trigger);
+        harvest.setBumpPower(gamepad1.right_trigger);
 
-        if (gamepad1.y) {
-            shootMode = 1;
-        } else if (gamepad1.x) {
-            shootMode = 2;
-        } else if (gamepad1.a) {
-            shootMode = 3;
-        } else if (gamepad1.b) {
-            shootMode = 0;
-        }
-
-        if (shootMode == 1) {
-            shoot.setPower(0.75);
-        } else if (shootMode == 2) {
-            shoot.setPower(0.2);
-        } else if (shootMode == 3) {
-            shoot.setPower(-0.2);
-        } else {
-            shoot.setPower(0);
-        }
+//        if (gamepad1.left_trigger > 0) {
+//            shoot.setServoPower(gamepad1.left_trigger);
+//        } else if (gamepad1.right_trigger > 0) {
+//            shoot.setServoPower(-gamepad1.right_trigger);
+//        } else {
+//            shoot.setServoPower(0);
+//        }
+//
+//        if (gamepad1.y) {
+//            shootMode = 1;
+//        } else if (gamepad1.x) {
+//            shootMode = 2;
+//        } else if (gamepad1.a) {
+//            shootMode = 3;
+//        } else if (gamepad1.b) {
+//            shootMode = 0;
+//        }
+//
+//        if (shootMode == 1) {
+//            shoot.setPower(0.75);
+//        } else if (shootMode == 2) {
+//            shoot.setPower(0.2);
+//        } else if (shootMode == 3) {
+//            shoot.setPower(-0.2);
+//        } else {
+//            shoot.setPower(0);
+//        }
     }
 
 }

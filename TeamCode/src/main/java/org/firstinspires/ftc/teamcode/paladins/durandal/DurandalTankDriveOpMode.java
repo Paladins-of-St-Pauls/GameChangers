f724d67a8c4837d5708ffd1d2ed6a3ba87fb4bbd
Levi Frizell <frizelr4927@stpauls.sa.edu.au>
2021-10-27 18:03:14 +1030

    The new code for the spinners on Durandal.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/paladins/durandal/DurandalTankDriveOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/paladins/durandal/DurandalTankDriveOpMode.java
@@ -2,19 +2,18 @@ package org.firstinspires.ftc.teamcode.paladins.durandal;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.paladins.common.ButtonControl;
+import org.firstinspires.ftc.teamcode.paladins.common.GamePadMomentaryMotor;
 import org.firstinspires.ftc.teamcode.paladins.common.PaladinsOpMode;
-import org.firstinspires.ftc.teamcode.paladins.joyeuse.JoyeuseConfiguration;
-import org.firstinspires.ftc.teamcode.paladins.joyeuse.JoyeuseDrive;
-import org.firstinspires.ftc.teamcode.paladins.joyeuse.JoyeuseGauntlet;
-import org.firstinspires.ftc.teamcode.paladins.joyeuse.JoyeuseIntake;
-import org.firstinspires.ftc.teamcode.paladins.joyeuse.JoyeuseShoot;
-import org.firstinspires.ftc.teamcode.paladins.joyeuse.JoyeuseTankDrive;
+
 
 @TeleOp(name = "DurandalTankDrive")
 public class DurandalTankDriveOpMode extends PaladinsOpMode {
     private DurandalConfiguration config;
     private DurandalDrive drive;
     private DurandalTankDrive tankDrive;
+    private GamePadMomentaryMotor leftSpinner;
+    private GamePadMomentaryMotor rightSpinner;
 
     @Override
     protected void onInit() {
@@ -22,13 +21,16 @@ public class DurandalTankDriveOpMode extends PaladinsOpMode {
 
         drive = new DurandalDrive(this, config.leftMotor, config.rightMotor);
         tankDrive = new DurandalTankDrive(this, gamepad1, drive);
-
+        leftSpinner = new GamePadMomentaryMotor(this, gamepad1, config.leftSpinnerMotor, ButtonControl.LEFT_BUMPER, 0.5f);
+        rightSpinner = new GamePadMomentaryMotor(this, gamepad1, config.rightSpinnerMotor, ButtonControl.RIGHT_BUMPER, 0.5f);
 
     }
 
     @Override
     protected void activeLoop() throws InterruptedException {
         tankDrive.update();
+        leftSpinner.update();
+        rightSpinner.update();
 
 
     }

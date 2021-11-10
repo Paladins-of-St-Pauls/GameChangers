5f26f34d70d248aa484c1049a4755a8b6328f028
Levi Frizell <frizelr4927@stpauls.sa.edu.au>
2021-11-10 17:33:31 +1030

    Harvester code is almost finished, we added functions including reversing and and minor speed changes.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/paladins/common/JoystickMomentaryServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/paladins/common/JoystickMomentaryServo.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode.paladins.common;
 
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -12,12 +11,12 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
  */
 public class JoystickMomentaryServo extends PaladinsComponent {
 
-    private final ButtonControl buttonControl;
-    private final JoystickControl joystickControl;
-
+    private final ButtonControl forwardButtonControl;
+    private final ButtonControl reverseButtonControl;
     private final CRServo motor;
     private final Gamepad gamepad;
-    private final float motorPower;
+    private final float forwardMotorPower;
+    private final float reverseMotorPower;
     private final Telemetry.Item item;
     private boolean showtelemetry = false;
 
@@ -26,32 +25,42 @@ public class JoystickMomentaryServo extends PaladinsComponent {
      * Constructor for operation.  Telemetry enabled by default.
      *
      * @param opMode
-     * @param gamepad         Gamepad
-     * @param motor           DcMotor to operate on
-     * @param buttonControl   {@link ButtonControl}
-     * @param joystickControl {@link JoystickControl}
-     * @param power           power to apply when using gamepad buttons
-     * @param showTelemetry   display the power values on the telemetry
+     * @param gamepad              Gamepad
+     * @param motor                DcMotor to operate on
+     * @param forwardButtonControl {@link ButtonControl}
+     * @param joystickControl      {@link JoystickControl}
+     * @param power                power to apply when using gamepad buttons
+     * @param showTelemetry        display the power values on the telemetry
      */
-    public JoystickMomentaryServo(PaladinsOpMode opMode, Gamepad gamepad, CRServo motor, ButtonControl buttonControl, float power, JoystickControl joystickControl, boolean showTelemetry) {
+    public JoystickMomentaryServo(PaladinsOpMode opMode, Gamepad gamepad, CRServo motor,
+                                  ButtonControl forwardButtonControl, float forwardPower,
+                                  ButtonControl reverseButtonControl, float reversePower,
+                                  boolean showTelemetry) {
         super(opMode);
 
         this.gamepad = gamepad;
         this.motor = motor;
-        this.buttonControl = buttonControl;
-        this.joystickControl = joystickControl;
-        this.motorPower = power;
+        this.forwardButtonControl = forwardButtonControl;
+        this.reverseButtonControl = reverseButtonControl;
+        this.forwardMotorPower = forwardPower;
+        this.reverseMotorPower = reversePower;
 
         if (showTelemetry) {
-            item = opMode.telemetry.addData("Control " + buttonControl.name(), 0.0f);
+            item = opMode.telemetry.addData("Control " + forwardButtonControl.name(), 0.0f);
             item.setRetained(true);
         } else {
             item = null;
         }
     }
 
-    public JoystickMomentaryServo(PaladinsOpMode opMode, Gamepad gamepad, CRServo motor, ButtonControl buttonControl, float power, JoystickControl joystickControl) {
-        this(opMode, gamepad, motor, buttonControl, power, joystickControl, true);
+    public JoystickMomentaryServo(PaladinsOpMode opMode, Gamepad gamepad, CRServo motor,
+                                  ButtonControl forwardButtonControl, float forwardPower,
+                                  ButtonControl reverseButtonControl, float reversePower) {
+        this(opMode, gamepad, motor, forwardButtonControl, forwardPower, reverseButtonControl, reversePower, true);
+    }
+    public JoystickMomentaryServo(PaladinsOpMode opMode, Gamepad gamepad, CRServo motor,
+                                  ButtonControl forwardButtonControl, float forwardPower) {
+        this(opMode, gamepad, motor, forwardButtonControl, forwardPower, ButtonControl.NONE, 0.0f, true);
     }
 
 
@@ -59,18 +68,13 @@ public class JoystickMomentaryServo extends PaladinsComponent {
      * Update motors with latest gamepad state
      */
     public void update() {
-        boolean pressed = ButtonControl.isSelected(gamepad, buttonControl);
-        float absJoystickValue = Math.abs(JoystickControl.getValue(gamepad, joystickControl));
-        float power = (pressed) ? motorPower : 0.0f;
-        if (absJoystickValue > Math.abs(power)) {
-            power = absJoystickValue * Math.signum(power);
-        }
+        boolean forwardPressed = ButtonControl.isSelected(gamepad, forwardButtonControl);
+        boolean reversePressed = ButtonControl.isSelected(gamepad, reverseButtonControl);
+        float power = (forwardPressed) ? forwardMotorPower : (reversePressed) ? reverseMotorPower : 0.0f;
         motor.setPower(power);
         if (showtelemetry) {
-            getOpMode().telemetry.log().add("%s motor power: %.2f", buttonControl.name(), power);
+            getOpMode().telemetry.log().add("%s motor power: %.2f", forwardButtonControl.name(), power);
         }
-
-
     }
 
 

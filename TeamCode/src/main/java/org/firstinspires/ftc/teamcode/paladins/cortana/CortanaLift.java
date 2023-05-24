af4c862e66c36e6b1398a199c3e7694cbd9e3c3d
Levi Frizell <frizelr4927@stpauls.sa.edu.au>
2023-05-24 15:57:30 +0930

    Change Lift back to how it was and start trying a new autonomous system

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/paladins/cortana/CortanaLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/paladins/cortana/CortanaLift.java
@@ -38,8 +38,8 @@ public class CortanaLift extends PaladinsComponent {
         liftClamp.scaleRange(0, 1);
         liftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         liftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         liftMotor.setTargetPosition(0);
         liftPower = 0;
     }
@@ -60,6 +60,23 @@ public class CortanaLift extends PaladinsComponent {
         liftMotor.setTargetPosition(lift_positions[liftIndex]);
     }
 
+    public void highGoal() {
+        liftMotor.setTargetPosition(lift_positions[9]);
+        liftIndex = 9;
+    }
+    public void midGoal() {
+        liftMotor.setTargetPosition(lift_positions[8]);
+        liftIndex = 8;
+    }
+    public void lowGoal() {
+        liftMotor.setTargetPosition(lift_positions[7]);
+        liftIndex = 7;
+    }
+
+    public void bottomOut() {
+        liftMotor.setTargetPosition(lift_positions[0]);
+        liftIndex = 0;
+    }
     public void liftClampOpen() {
         liftClamp.setPosition(0.2);
     }
@@ -67,9 +84,10 @@ public class CortanaLift extends PaladinsComponent {
         liftClamp.setPosition(0.7);
     }
 
-    public void liftReset() {
-        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    }
+//    public void liftReset() {
+//        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        liftMotor.setPower(0);
+//    }
 
     public void setPower(double power) {
         liftPower = power;
@@ -82,16 +100,16 @@ public class CortanaLift extends PaladinsComponent {
     @SuppressLint("DefaultLocale")
     public void update() {
         getOpMode().telemetry.addLine(String.format("Lift Position: %d",liftMotor.getCurrentPosition()));
-
-        if(liftIndex == 0 && liftSensor.isPressed()) {
-            liftReset();
-        }
-        else {
+//
+//        if(liftSensor.isPressed()) {
+//            liftMotor
+//        }
+//        else {
             liftMotor.setPower((liftPower));
-            liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//            liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         }
 
-    }
+//    }
 
     public boolean isFinished() {
         return !(liftMotor.isBusy());
